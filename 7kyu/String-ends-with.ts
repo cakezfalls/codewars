8b6a2827e0e998be7500849d847753ad2f2eeffd
Alex Usachev <hootsafer@gmail.com>
2025-02-20 17:22:08 +0400

    Day 11

---
+++ 7kyu/String-ends-with.ts
@@ -0,0 +1,7 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//My Soluiton:
+
+export function solution(str: string, ending: string): boolean {
+    return str.endsWith(ending);
+  }
