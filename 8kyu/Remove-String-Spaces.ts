676e70fb12c50e53f5fa5155a1539261e49153d4
Alex Usachev <hootsafer@gmail.com>
2025-03-01 17:32:01 +0400

    Day 20

---
+++ 8kyu/Remove-String-Spaces.ts
@@ -0,0 +1,7 @@
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+//My Solution:
+
+export function noSpace(x:string):string {
+    return x.replaceAll(' ','')
+  }
