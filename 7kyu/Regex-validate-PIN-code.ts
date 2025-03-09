3a18674f969e776782b987276e1c280d71b0bdd6
Alex Usachev <hootsafer@gmail.com>
2025-03-09 22:26:30 +0400

    Day 27

---
+++ 7kyu/Regex-validate-PIN-code.ts
@@ -0,0 +1,12 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+//My Solution:
+
+export class Kata {
+    static validatePin(pin: string): boolean {
+      const newArray = pin.split('').map(Number); 
+       return (newArray.every(i => !isNaN(i)) && (newArray.length === 4 || newArray.length === 6)) 
+    }
+  }
