5c4078757b37c27aefcb2e455ce11e79d85414fb
Alex Usachev <hootsafer@gmail.com>
2025-03-24 17:50:36 +0400

    Day 39

---
+++ 7kyu/List-Filtering.ts
@@ -0,0 +1,8 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and 
+// returns a new list with the strings filtered out.
+
+//My Solution:
+
+export function filter_list(l:Array<any>):Array<number> {
+    return l.filter (i => typeof i === 'number');
+  }
