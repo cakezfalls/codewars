c5963c6070811ab56605faab61e2591a0c445667
Alex Usachev <hootsafer@gmail.com>
2025-03-25 22:37:37 +0400

    Day 40

---
+++ 6kyu/Word-a10n-(abbreviation).ts
@@ -0,0 +1,15 @@
+// The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+// Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+
+// A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+// The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+
+
+//My Solution:
+
+export function abbreviate(str: string): string {
+    return str.replace(/[a-zA-Z]{4,}/g, (word) => {
+     return word[0] + (word.length - 2) + word[word.length - 1];
+   });
+ }
