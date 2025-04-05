15d9c5ec62204e6976d5422d3d6f06e9f980315b
Alex Usachev <hootsafer@gmail.com>
2025-04-05 21:49:12 +0400

    Day 50

---
+++ 7kyu/Friend-or-Foe.ts
@@ -0,0 +1,15 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []
+
+//My Solution:
+
+export function friend(friends: string[]): string[] {
+  return friends.filter((name) => name.length === 4);
+}
